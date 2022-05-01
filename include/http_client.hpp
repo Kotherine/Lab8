5b052fe5f87108a3a55bbb456d30acf76cf4a4a9
Kotherine <astapchukep@student.bmstu.ru>
2022-05-01 13:12:05 +0300

    final version

---
+++ include/http_client.hpp
@@ -0,0 +1,8 @@
+// Copyright 2021 Your Name <your_email>
+
+#ifndef INCLUDE_HTTP_CLIENT_HPP_
+#define INCLUDE_HTTP_CLIENT_HPP_
+
+auto example() -> void;
+
+#endif // INCLUDE_HTTP_CLIENT_HPP_
--- sources/http_client.cpp
+++ sources/http_client.cpp
@@ -1,8 +1,7 @@
 // Copyright 2021 Your Name <your_email>
 
 #include <stdexcept>
-
-#include <example.hpp>
+#include <http_client.hpp>
 
 auto example() -> void {
   throw std::runtime_error("not implemented");
